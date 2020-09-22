dfbe0af1c60169364ae4e81a2f6a1aaac9fa6330
Mathias Louboutin <mathias.louboutin@gmail.com>
2020-09-22 15:31:52 -0400

    compare with torch

--- test.py
+++ test.py
@@ -1,37 +1,68 @@
 import numpy as np
 from devito import *
 
+import torch
+import torch.nn as nn
 
-# Image size
-dt = np.float64
-nx, ny, nch = 1024, 1024, 4
-x, y, c = SpaceDimension("x"), SpaceDimension("y"), SpaceDimension("c")
-grid = Grid((nx, ny, nch), dtype=dt, dimensions=(x, y, c))
 
-stride = 2
+@profile
+def conv(nx, ny, nch, n, m):
+    # Image size
+    dt = np.float64
+    x, y, c = SpaceDimension("x"), SpaceDimension("y"), SpaceDimension("c")
+    grid = Grid((nch, nx, ny), dtype=dt, dimensions=(c, x, y))
 
-# Image
-im_in = Function(name="imi", grid=grid, space_order=1)
-im_in.data[:, :] = np.linspace(-1, 1, nx*ny).reshape(nx, ny)
+    stride = 2
 
-# Output
-im_out = Function(name="imo", grid=grid, space_order=1)
+    # Image
+    im_in = Function(name="imi", grid=grid, space_order=1)
+    im_in.data[:, :, :] = np.linspace(-1, 1, nx*ny*nch).reshape(nch, nx, ny)
 
-# Weights
-i, j = Dimension("i"), Dimension("j")
-n, m = 3, 3
-W = Function(name="W", dimensions=(i, j, c), shape=(n, m, nch), grid=grid)
-# popuate weights with deterministic values
-for i in range(nch):
-    W.data[:, :, i] = np.linspace(i, i+(n*m), n*m).reshape(n, m)
+    # Output
+    im_out = Function(name="imo", grid=grid, space_order=1)
+    im_out.data
 
-# Convlution
-conv = sum([W[i1, i2, c]*im_in[x+i1-n//2, y+i2-m//2, c]
-            for i1 in range(n) for i2 in range(m)])
+    # Weights
+    i, j = Dimension("i"), Dimension("j")
+    W = Function(name="W", dimensions=(i, j, c), shape=(n, m, nch), grid=grid)
+    # popuate weights with deterministic values
+    for i in range(nch):
+        W.data[:, :, i] = np.linspace(i, i+(n*m), n*m).reshape(n, m)
 
-op = Operator(Eq(im_out, conv))
-op()
+    # Convlution
+    conv = sum([W[i2, i1, c]*im_in[c, x+i1-n//2, y+i2-m//2]
+                for i1 in range(n) for i2 in range(m)])
 
-# then return im_our.data[::stride, ::stride] .... if stride, and batchsize jut another dim like 6/7
+    op = Operator(Eq(im_out, conv))
+    op()
 
-print(norm(im_out))
+    # then return im_our.data[::stride, ::stride] .... if stride, and batchsize jut another dim like 6/7
+
+    print(norm(im_out))
+    return im_out.data
+
+
+@profile
+def conv_torch(nx, ny, nch, n, m):
+    convt = nn.Conv2d(nch, nch, (n, m), stride=(1, 1), padding=(1, 1), bias=False)
+
+    ww = np.zeros((nch, nch, n, m), dtype=np.float32)
+    for i in range(nch):
+        ww[i, i, :, :] = np.linspace(i, i+(n*m), n*m).reshape(n, m).T
+
+    convt.weight[:] = torch.from_numpy(ww)
+
+    in_array = np.linspace(-1, 1, nx*ny*nch).reshape(1, nch, nx, ny).astype(np.float32)
+    im_in = torch.from_numpy(in_array)
+    im_out = convt(im_in)
+    print(np.linalg.norm(im_out.detach().numpy()))
+    return im_out.detach().numpy()
+
+
+if __name__ == '__main__':
+    nx, ny, nch = 1024, 1024, 4
+    n, m = 3, 3
+    res1 = conv(nx, ny, nch, n, m)
+    res2 = conv_torch(nx, ny, nch, n, m)
+    err = np.linalg.norm(res1 - res2)/np.linalg.norm(res1)
+    print("Difference between devito and pytorch is %2.2e \n" % err)
--- test3d.py
+++ test3d.py
@@ -2,36 +2,41 @@ import numpy as np
 from devito import *
 
 
-# Image size
-dt = np.float32
-nx, ny, nz, nch = 256, 256, 128, 4
-x, y, z, c = SpaceDimension("x"), SpaceDimension("y"), SpaceDimension("z"), SpaceDimension("c")
-grid = Grid((nx, ny, nz, nch), dimensions=(x,y,z,c), dtype=dt)
+@profile
+def conv():
+    # Image size
+    dt = np.float32
+    nx, ny, nz, nch = 256, 256, 128, 4
+    x, y, z, c = SpaceDimension("x"), SpaceDimension("y"), SpaceDimension("z"), SpaceDimension("c")
+    grid = Grid((nx, ny, nz, nch), dimensions=(x,y,z,c), dtype=dt)
 
-stride = 2
+    stride = 2
 
-# Image
-im_in = Function(name="imi", grid=grid, space_order=1)
-im_in.data[:, :] = np.linspace(-1, 1, nx*ny*nz).reshape(nx, ny, nz)
+    # Image
+    im_in = Function(name="imi", grid=grid, space_order=1)
+    im_in.data[:, :] = np.linspace(-1, 1, nx*ny*nz).reshape(nx, ny, nz)
 
-# Output
-im_out = Function(name="imo", grid=grid, space_order=1)
+    # Output
+    im_out = Function(name="imo", grid=grid, space_order=1)
 
-# Weights
-i, j, k = Dimension("i"), Dimension("j"), Dimension("k")
-l, m, n = 3, 3, 3
-W = Function(name="W", dimensions=(i, j, k, c), shape=(l, m, n, nch), grid=grid)
-# popuate weights with deterministic values
-for i in range(nch):
-    W.data[:, :, :, i] = np.linspace(i, i+(l*m*n), l*m*n).reshape(l, m, n)
+    # Weights
+    i, j, k = Dimension("i"), Dimension("j"), Dimension("k")
+    l, m, n = 3, 3, 3
+    W = Function(name="W", dimensions=(i, j, k, c), shape=(l, m, n, nch), grid=grid)
+    # popuate weights with deterministic values
+    for i in range(nch):
+        W.data[:, :, :, i] = np.linspace(i, i+(l*m*n), l*m*n).reshape(l, m, n)
 
-# Convlution
-conv = sum([W[i1, i2, i3, c]*im_in[x+i1-l//2, y+i2-m//2, z+i3-n//2, c]
-            for i1 in range(l) for i2 in range(m) for i3 in range(n)])
+    # Convlution
+    conv = sum([W[i1, i2, i3, c]*im_in[x+i1-l//2, y+i2-m//2, z+i3-n//2, c]
+                for i1 in range(l) for i2 in range(m) for i3 in range(n)])
 
-op = Operator(Eq(im_out, conv))
-op()
+    op = Operator(Eq(im_out, conv))
+    op()
 
-# then return im_our.data[::stride, ::stride] .... if stride, and batchsize jut another dim like 6/7
+    # then return im_our.data[::stride, ::stride] .... if stride, and batchsize just another dim like 6/7
 
-print(norm(im_out))
+    print(norm(im_out))
+
+if __name__ == '__main__':
+    conv()
